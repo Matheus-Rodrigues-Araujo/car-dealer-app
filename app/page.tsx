81cc7f4dbcd3135defe73dda3d6e20e75fdbe932
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-10-16 21:05:32 -0300

    beta ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,86 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [data, setData] = useState([]);
+  const url = process.env.NEXT_PUBLIC_MAKE_URL || "";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [makeValue, setMakeValue] = useState("");
+  const [modelYear, setModelYear] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
+  const years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+
+  const fetchData = async () => {
+    const response = await axios.get(url);
+    const responseData = await response.data;
+    setData(responseData["Results"]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  useEffect(() => {
+    if (!makeValue.length || !modelYear.length) {
+      setIsActive(false);
+    } else {
+      setIsActive(true);
+    }
+  }, [makeValue, modelYear]);
+
+  return (
+    <div className="max-w-xl h-54 p-8 grid gap-5 rounded-lg bg-white">
+      <div className="grid grid-cols-2 gap-5">
+        <div className="flex flex-col gap-4">
+          <label className="font-bold">Choose a car:</label>
+          <select
+            name="cars"
+            id="cars"
+            onChange={(event) => setMakeValue(event.target.value)}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {data &&
+              data.map((item, key) => (
+                <option
+                  key={key}
+                  value={item["MakeId"]}
+                  onChange={(event) => console.log(event.currentTarget)}
+                >
+                  {item["MakeId"]} | {item["MakeName"]}
+                </option>
+              ))}
+          </select>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <div className="flex flex-col gap-4">
+          <label className="font-bold">Choose a Model Year:</label>
+          <select
+            name="modelYear"
+            id="modelYear"
+            onChange={(event) => setModelYear(event.target.value)}
+          >
+            {years.map((item, key) => (
+              <option key={key} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      {isActive ? (
+        <Link
+          href={`/result/${makeValue}/${modelYear}`}
+          className="bg-cyan-400 hover:bg-cyan-500 text-white text-center font-bold p-1 w-3/12 ml-auto"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Next
+        </Link>
+      ) : (
+        <span className="bg-gray-400 hover:bg-gray-500 text-white text-center font-bold p-1 w-3/12 ml-auto">
+          Not Allowed
+        </span>
+      )}
     </div>
   );
 }
---
+++ app/result/[id]/[year]/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const Result = ({ params }: { params: { id: string; year: string } }) => {
+  const { id, year } = params;
+  const [vehicle, setVehicle] = useState([]);
+
+  const url = `${process.env.NEXT_PUBLIC_VEHICLE_URL}/makeId/${id}/modelyear/${year}?format=json`;
+
+  const fetchVehicle = async () => {
+    try {
+      const response = await axios.get(url);
+      const responseData = await response.data;
+      setVehicle(responseData["Results"]);
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchVehicle();
+  });
+
+  return (
+    <div className="p-8 grid gap-5 rounded-lg bg-white">
+      <h1 className="text-xl font-bold" >Vechicle Data</h1>
+      <h2 className="text-lg font-medium" >ID: {id} | YEAR: {year}</h2>
+      <div className="flex flex-wrap gap-2">
+        {vehicle &&
+          vehicle.map((item, key) => (
+            <div key={key} className="w-64 border border-1 p-2">
+              <div>
+                <p>MAKE_ID: {item["Make_Name"]}</p>
+                <p>MAKE_NAME: {item["Make_Name"]}</p>
+                <p>MODEL_ID: {item["Model_ID"]}</p>
+                <p>MODEL_NAME: {item["Model_Name"]}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Result;
